83eb7cb1b0d64a2639a16796d48729cb165517ec
Ken Elliott <kenelliottmc@gmail.com>
2026-08-18 09:11:57 -0500

    Register August 16 meal plan

--- src/data/plans/index.js
+++ src/data/plans/index.js
@@ -1,6 +1,7 @@
+import * as plan_2026_08_16 from "./2026-08-16";
 import * as plan_2026_07_05 from "./2026-07-05";
 import * as plan_2026_07_19 from "./2026-07-19";
 
-const plans = [plan_2026_07_19, plan_2026_07_05];
+const plans = [plan_2026_08_16, plan_2026_07_19, plan_2026_07_05];
 
 export default plans;
